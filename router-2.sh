7865613cd73de4abf969901d8fb36a21332ffab9
Fermati <44473791+Fermati@users.noreply.github.com>
2019-02-10 15:06:03 +0100

    Update router-2.sh

--- router-2.sh
+++ router-2.sh
@@ -5,3 +5,7 @@ wget -O- https://apps3.cumulusnetworks.com/setup/cumulus-apps-deb.pubkey | apt-k
 add-apt-repository "deb [arch=amd64] https://apps3.cumulusnetworks.com/repos/deb $(lsb_release -cs) roh-3"
 apt-get update
 apt-get install -y frr --assume-yes --force-yes
+ip addr add 192.168.3.2 dev eth2
+ip addr add 192.168.4.1 dev eth1
+ip link set eth2 up
+ip link set eth1 up

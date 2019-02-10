12333b1202acb62cfdf02638727530bb5c65bc99
Fermati <44473791+Fermati@users.noreply.github.com>
2019-02-10 13:33:20 +0100

    Update host-1-a.sh

--- host-1-a.sh
+++ host-1-a.sh
@@ -1,1 +1,5 @@
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y tcpdump --assume-yes
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common --assume-yes --force-yes
 

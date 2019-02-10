efa956f993471f4e640f1654a8e68c85bdcf2123
Fermati <44473791+Fermati@users.noreply.github.com>
2019-02-10 16:13:21 +0100

    Update host-2-c.sh

--- host-2-c.sh
+++ host-2-c.sh
@@ -2,3 +2,5 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y tcpdump --assume-yes
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common --assume-yes --force-yes
+ip addr add 192.168.4.2
+ip link set eth1 up

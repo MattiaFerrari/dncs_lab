9f64d4ec76988e03429b51ea9158a031a9da5b9d
Fermati <44473791+Fermati@users.noreply.github.com>
2019-02-12 15:15:17 +0100

    Aggiungere static route

--- host-1-b.sh
+++ host-1-b.sh
@@ -2,5 +2,6 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y tcpdump --assume-yes
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common --assume-yes --force-yes
-ip addr add 192.168.2.30 dev eth1
 ip link set eth1 up
+ip addr add 192.168.1.1/27 dev eth1
+ip replace XXX.XX.X.X/XX via 163.168.0.31

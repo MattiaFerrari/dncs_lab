7509bbd712f57986e2c99e2e88194badfa4bd49d
Fermati <44473791+Fermati@users.noreply.github.com>
2019-02-12 15:06:54 +0100

    Solo da Testare

--- switch.sh
+++ switch.sh
@@ -2,10 +2,12 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y tcpdump --assume-yes
 apt-get install -y openvswitch-common openvswitch-switch apt-transport-https ca-certificates curl software-properties-common
+
 ovs-vsctl add-br switch
 ovs-vsctl add-port switch eth 1
 ovs-vsctl add-port switch eth 2 tag=1
 ovs-vsctl add-port switch eth 3 tag=2
+
 ip link set eth1 up
 ip link set eth 2 up
 ip link set eth 3 up

f9106b6d1a9502f8a2ff93ff6b25dd1619eecf91
Fermati <44473791+Fermati@users.noreply.github.com>
2019-02-12 15:11:30 +0100

    Aggiungere static route

--- host-1-a.sh
+++ host-1-a.sh
@@ -4,3 +4,4 @@ apt-get install -y tcpdump --assume-yes
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common --assume-yes --force-yes
 ip addr add 192.168.1.254 dev eth1
 ip link set eth1 up
+ip route replace XXX.XX.X.X/XX via 163.10.0.254

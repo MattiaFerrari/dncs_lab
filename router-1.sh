eeaf13489ced12ddb8cf78023e175f1e1045ec51
Fermati <44473791+Fermati@users.noreply.github.com>
2019-02-12 14:57:18 +0100

    Update router-1.sh

--- router-1.sh
+++ router-1.sh
@@ -5,15 +5,20 @@ wget -O- https://apps3.cumulusnetworks.com/setup/cumulus-apps-deb.pubkey | apt-k
 add-apt-repository "deb [arch=amd64] https://apps3.cumulusnetworks.com/repos/deb $(lsb_release -cs) roh-3"
 apt-get update
 apt-get install -y frr --assume-yes --force-yes
-ip link add link eth1 name eth1.1type vlan id 1
-ip link add link eth1 name eth1.1type vlan id 1
+
+ip link set eth1 up
+ip link add link eth1 name eth1.1 type vlan id 1
+ip link add link eth1 name eth1.2 type vlan id 2
+
 ip addr add 192.168.1.1/24 dev eth1.1
 ip addr add 192.168.2.1/24 dev eth1.2
-ip addr add 192.168.3.1 dev eth2
-ip link set eth1 up
+
 ip link set eth1.1 up
 ip link set eth1.2 up
+
+ip addr add 192.168.3.1 dev eth2
 ip link set eth2 up
+
 sysctl net.ipv4.ip_forward=1
 sed -i 's/zebra=no/zebra=yes/g' /etc/frr/daemons
 sed -i 's/ospfd=no/ospfd=yes/g' /etc/frr/daemons
